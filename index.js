7206944b23c35445f38f482b39044f66a3394b6f
Luis Henrique Arandas <luis.arandas@gmail.com>
2020-09-30 10:05:08 +0100

    new pages

---
+++ index.js
@@ -0,0 +1,22 @@
+console.log("ola");
+
+const btn1 = document.getElementById('btn1');
+
+btn1.addEventListener("click", function() {
+  console.log("joaquim");
+  window.location.href='catalog.html'
+});
+
+const btn2 = document.getElementById('btn2');
+
+btn2.addEventListener("click", function() {
+  console.log("joaquim");
+  window.location.href='augmented.html'
+});
+
+const btn3 = document.getElementById('btn3');
+
+btn3.addEventListener("click", function() {
+  console.log("joaquim");
+  window.location.href='virtual.html'
+});
