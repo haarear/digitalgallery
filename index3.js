d103e985f90081392c0bce2fc2432e8ad6b6fdac
Luis Henrique Arandas <luis.arandas@gmail.com>
2020-09-30 17:31:53 +0100

    teste de mexer

--- index3.js
+++ index3.js
@@ -38,18 +38,3 @@ document.addEventListener ('keypress', (event) => {
 	console.log("ika");*/
 	};
 });
-
-<<<<<<< HEAD
-
-var targetEl = document.querySelector('#geo1');
-targetEl.addEventListener('click', function() {
-  targetEl.setAttribute('material', {color: 'red'});
-});
-=======
-var geoEl = document.querySelector('#geo1');
-
-geoEl.addEventListener("click", function () {
-  geoEl.setAttribute('material', {color: 'red'});
-  alert('hi!');
-})
->>>>>>> ae9ad4552744b3908f50318fc6d547e9708ebcc9
