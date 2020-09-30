e688ad8ec88663c538575bb796e6e573261c2f22
Luis Henrique Arandas <luis.arandas@gmail.com>
2020-09-30 16:00:39 +0100

    +

--- index3.js
+++ index3.js
@@ -7,6 +7,6 @@ document.getElementById('geo1').addEventListener('click', function (evt) {
   alert("hi!");
 });
 
-document.querySelector('geo1').addEventListener('physicscollided', function (event) {
+document.getElementById('geo1').addEventListener('physicscollided', function (event) {
   console.log('Entity collided with', event.detail.collidingEntity);
 });
