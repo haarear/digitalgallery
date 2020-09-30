e64eb3cae71eece1ee22c14cc8891585cf60d039
Luis Henrique Arandas <luis.arandas@gmail.com>
2020-09-30 15:50:14 +0100

    test branch

--- index3.js
+++ index3.js
@@ -1,4 +1,8 @@
 // `click` event emitted by browser on mouse click.
-document.querySelector('geo1').addEventListener('ontouchstart', function (evt) {
+document.getElementById('geo1').addEventListener('ontouchstart', function (evt) {
+  alert("hi!");
+});
+
+document.getElementById('geo1').addEventListener('click', function (evt) {
   alert("hi!");
 });
